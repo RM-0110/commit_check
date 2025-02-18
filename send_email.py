87d6c635405e34bff1b732b31c7b1a9d4afc0dfc
Riddhimann Mukherjee <138844240+RM-0110@users.noreply.github.com>
2025-02-18 19:40:17 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -213,7 +213,7 @@ email_body += "\n\n\n"
 email_body += "Build numbers having 'none' value indicates that the latest preprod deployment does not have any upstream project linked to it."
 
 sender_email = "riddhimann@navyatech.in"  # Replace with your email
-receiver_emails = ["riddhimann@navyatech.in", "kirana@navyatech.in"]  # Replace with your email
+receiver_emails = ["riddhimann@navyatech.in", "kirana@navyatech.in", "pushpa@navyatech.in", "armugam@navyatech.in"]  # Replace with your email
 password = os.getenv('APP_PASSWORD')
 
 subject = "Daily Commit List - Preprod - " + str(formatted_time)
